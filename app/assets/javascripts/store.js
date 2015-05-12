83b187d32549627bab06c67e4ba22d2d9a50704f
Leah LaSalla <leah@opensorceress.net>
2015-05-11 23:53:38 -0500

    refactored js

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,5 +1,11 @@
-$(document).on("ready page:change", function() {
-  return $('.store .entry > img').click(function() {
-    return $(this).parent().find(':submit').click();
-  });
+//$(document).on("ready page:change", function() {
+//  return $('.store .entry > img').click(function() {
+//    return $(this).parent().find(':submit').click();
+//  });
+//});
+
+$('.entry>img').on('click', function() {
+    
+    $(this).closest('.entry').find(':submit').click();
+
 });
